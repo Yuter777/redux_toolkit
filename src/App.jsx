5ac939fb6c4cea361e86863e87067a6b6acbc573
Muhammad Tursunxojayev <147787386+Yuter777@users.noreply.github.com>
2024-08-04 19:20:28 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,4 @@
-// import React, { useState } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Layout } from "antd";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Students from "./pages/Students";
-// import Teachers from "./pages/Teachers";
-// import User from "./pages/User";
-// import Login from "./pages/Login";
 
-// const { Content, Sider } = Layout;
-
-// function App() {
-//   const [students, setStudents] = useState(0);
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   return (
-//     <Router>
-//       <Layout style={{ minHeight: "100vh" }}>
-//         <Sider>
-//           <Navbar isLoggedIn={isLoggedIn} />
-//         </Sider>
-//         <Layout className="site-layout">
-//           <Content style={{ margin: "16px" }}>
-//             <Routes>
-//               <Route path="/" element={<Home />} />
-//               <Route path="/about" element={<About />} />
-// <Route
-//   path="/students"
-//   element={
-//     isLoggedIn ? (
-//       <Students students={students} setStudents={setStudents} />
-//     ) : (
-//       <Login setIsLoggedIn={setIsLoggedIn} />
-//     )
-//   }
-// />
-// <Route
-//   path="/teachers"
-//   element={
-//     isLoggedIn ? (
-//       <Teachers />
-//     ) : (
-//       <Login setIsLoggedIn={setIsLoggedIn} />
-//     )
-//   }
-// />
-//               <Route path="/user" element={<User />} />
-//               <Route
-//                 path="/login"
-//                 element={<Login setIsLoggedIn={setIsLoggedIn} />}
-//               />
-//             </Routes>
-//           </Content>
-//         </Layout>
-//       </Layout>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -69,7 +8,7 @@ import About from "./pages/About";
 import Students from "./pages/Students";
 import Teachers from "./pages/Teachers";
 import User from "./pages/User";
-import AdminPanel from "./pages/AdminPanel";
+
 import Login from "./pages/Login";
 
 const App = () => {
