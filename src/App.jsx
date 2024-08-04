d1722e0b055abd3a2c7dd15a7ebf11a8e1fddc42
Muhammad Tursunxojayev <147787386+Yuter777@users.noreply.github.com>
2024-08-04 19:45:33 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -8,43 +7,56 @@ import About from "./pages/About";
 import Students from "./pages/Students";
 import Teachers from "./pages/Teachers";
 import User from "./pages/User";
-
 import Login from "./pages/Login";
+import { Layout } from "antd";
+import Sider from "antd/es/layout/Sider";
+import Navbar from "./components/Navbar";
+import { Content } from "antd/es/layout/layout";
 
 const App = () => {
+  const [students, setStudents] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Provider store={store}>
       <Router>
-        <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route
-            path="/students"
-            element={
-              isLoggedIn ? (
-                <Students students={students} setStudents={setStudents} />
-              ) : (
-                <Login setIsLoggedIn={setIsLoggedIn} />
-              )
-            }
-          />
-          <Route
-            path="/teachers"
-            element={
-              isLoggedIn ? (
-                <Teachers />
-              ) : (
-                <Login setIsLoggedIn={setIsLoggedIn} />
-              )
-            }
-          />{" "}
-          <Route path="/user" component={User} />
-          <Route path="/admin" component={AdminPanel} />
-          <Route
-            path="/login"
-            element={<Login setIsLoggedIn={setIsLoggedIn} />}
-          />
-        </Routes>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Sider>
+            <Navbar isLoggedIn={isLoggedIn} />
+          </Sider>
+          <Layout className="site-layout">
+            <Content style={{ margin: "16px" }}>
+              <Routes>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route
+                  path="/students"
+                  element={
+                    isLoggedIn ? (
+                      <Students students={students} setStudents={setStudents} />
+                    ) : (
+                      <Login setIsLoggedIn={setIsLoggedIn} />
+                    )
+                  }
+                />
+                <Route
+                  path="/teachers"
+                  element={
+                    isLoggedIn ? (
+                      <Teachers />
+                    ) : (
+                      <Login setIsLoggedIn={setIsLoggedIn} />
+                    )
+                  }
+                />{" "}
+                <Route path="/user" component={User} />
+                <Route
+                  path="/login"
+                  element={<Login setIsLoggedIn={setIsLoggedIn} />}
+                />
+              </Routes>{" "}
+            </Content>
+          </Layout>
+        </Layout>
       </Router>
     </Provider>
   );
